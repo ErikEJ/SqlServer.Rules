874ec2634ca16ac8fc3153611d4096186817addf
Erik Ejlskov Jensen <ErikEJ@users.noreply.github.com>
2025-03-19 10:40:06 +0000

    Consider COLUMNSTORE indexes in related rules (#170)
    * Consider COLUMNSTORE indexes in related rules
    
    fixes #155
    
    * Fix docs

---
+++ test/SqlServer.Rules.Test/Design/SRD0002Tests.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelpers;
+
+namespace SqlServer.Rules.Tests.Design;
+
+[TestClass]
+public class SRD0002Tests : TestModel
+{
+    public SRD0002Tests()
+        : base(TestConstants.SqlServerRules)
+    {
+    }
+
+    [TestMethod]
+    public void MustHavePrimaryKey()
+    {
+        TestFiles.Add("../../../../../sqlprojects/TSQLSmellsTest/CreateTableNoSchema.sql");
+        TestFiles.Add("../../../../../sqlprojects/TSQLSmellsTest/CreateTableClusteredColumnStore.sql");
+
+        ExpectedProblems.Add(new TestProblem(1, 1, "SqlServer.Rules.SRD0002"));
+        ExpectedProblems.Add(new TestProblem(1, 1, "SqlServer.Rules.SRD0067"));
+        ExpectedProblems.Add(new TestProblem(1, 1, "SqlServer.Rules.SRD0067"));
+        ExpectedProblems.Add(new TestProblem(1, 1, "SqlServer.Rules.SRN0006"));
+        ExpectedProblems.Add(new TestProblem(1, 1, "SqlServer.Rules.SRP0020"));
+
+        RunTest();
+    }
+}
