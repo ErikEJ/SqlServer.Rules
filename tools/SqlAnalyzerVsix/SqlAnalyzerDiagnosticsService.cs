591513544537e216ef06a1791ef067c46d03d3b4
Erik Ejlskov Jensen <eej@delegate.dk>
2025-08-24 09:01:14 +0200

    add readme and refactor DRY

--- tools/SqlAnalyzerVsix/SqlAnalyzerDiagnosticsService.cs
+++ tools/SqlAnalyzerVsix/SqlAnalyzerDiagnosticsService.cs
@@ -2,6 +2,7 @@ namespace SqlAnalyzer;
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft;
@@ -9,6 +10,7 @@ using Microsoft.VisualStudio.Extensibility;
 using Microsoft.VisualStudio.Extensibility.Documents;
 using Microsoft.VisualStudio.Extensibility.Editor;
 using Microsoft.VisualStudio.Extensibility.Helpers;
+using Microsoft.VisualStudio.ProjectSystem.Query;
 using Microsoft.VisualStudio.Threading;
 
 /// <summary>
@@ -71,7 +73,9 @@ internal class SqlAnalyzerDiagnosticsService : DisposableObject
 
         try
         {
-            var diagnostics = await this.analyzerUtilities.RunAnalyzerOnFileAsync(documentUri, cancellationToken);
+            var runProperties = await this.IsInSqlProjAsync(documentUri.LocalPath, cancellationToken);
+
+            var diagnostics = await this.analyzerUtilities.RunAnalyzerOnFileAsync(documentUri, runProperties.Rules, runProperties.SqlVersion, cancellationToken);
 
             await this.diagnosticsReporter!.ClearDiagnosticsAsync(documentUri, cancellationToken);
             await this.diagnosticsReporter!.ReportDiagnosticsAsync(diagnostics, cancellationToken);
@@ -89,11 +93,9 @@ internal class SqlAnalyzerDiagnosticsService : DisposableObject
     /// Processes the current version <see cref="ITextViewSnapshot"/> instance for SQL errors and reports to the error list.
     /// </summary>
     /// <param name="textViewSnapshot">Text View instance to read the contents from.</param>
-    /// <param name="rules">Rules to apply for the analyzer.</param>
-    /// <param name="sqlVersion">SQL version to use for analysis.</param>
     /// <param name="cancellationToken">Cancellation token to monitor.</param>
     /// <returns>Task indicating completion of reporting SQL errors to error list.</returns>
-    public async Task ProcessTextViewAsync(ITextViewSnapshot textViewSnapshot, string? rules, string? sqlVersion, CancellationToken cancellationToken)
+    public async Task ProcessTextViewAsync(ITextViewSnapshot textViewSnapshot, CancellationToken cancellationToken)
     {
         CancellationTokenSource newCts = new CancellationTokenSource();
         lock (this.documentCancellationTokens)
@@ -106,7 +108,11 @@ internal class SqlAnalyzerDiagnosticsService : DisposableObject
             this.documentCancellationTokens[textViewSnapshot.Document.Uri] = newCts;
         }
 
-        await this.ProcessDocumentAsync(textViewSnapshot.Document, rules, sqlVersion, cancellationToken.CombineWith(newCts.Token).Token);
+        var runProperties = await this.IsInSqlProjAsync(textViewSnapshot.Uri.LocalPath, cancellationToken);
+        if (runProperties.Run)
+        {
+            await this.ProcessDocumentAsync(textViewSnapshot.Document, runProperties.Rules, runProperties.SqlVersion, cancellationToken.CombineWith(newCts.Token).Token);
+        }
     }
 
     /// <summary>
@@ -172,6 +178,39 @@ internal class SqlAnalyzerDiagnosticsService : DisposableObject
         this.outputChannel = await this.extensibility.Views().Output.CreateOutputChannelAsync(Strings.MarkdownLinterWindowName, default);
         this.diagnosticsReporter = this.extensibility.Languages().GetDiagnosticsReporter(nameof(SqlAnalyzerExtension));
         Assumes.NotNull(this.diagnosticsReporter);
+    }
+
+    private async Task<(bool Run, string? Rules, string? SqlVersion)> IsInSqlProjAsync(string path, CancellationToken cancellationToken)
+    {
+        var workspace = this.extensibility.Workspaces();
+
+#pragma warning disable VSEXTPREVIEW_PROJECTQUERY_PROPERTIES_BUILDPROPERTIES // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+        IQueryResults<IProjectSnapshot> projects = await workspace.QueryProjectsAsync(
+            project => project.Where(p => p.Capabilities.Contains(SqlAnalyzerExtension.SqlProjCapability))
+            .WithRequired(project => project.FilesByPath(path))
+            .With(p => p.ActiveConfigurations
+                .With(c => c.PropertiesByName(PropertySourceType.ProjectFile, "RunSqlCodeAnalysis", "CodeAnalysisRules", "SqlServerVersion"))),
+            cancellationToken);
+
+        var runAnalyzer = projects.Any(p => p.ActiveConfigurations.Any(c =>
+            c.Properties.Any(prop =>
+                prop.Name.Equals("RunSqlCodeAnalysis", StringComparison.OrdinalIgnoreCase)
+                && prop.Value != null
+                && prop.Value.Equals("true", StringComparison.OrdinalIgnoreCase))));
+
+        var rules = projects.SelectMany(p => p.ActiveConfigurations)
+            .SelectMany(c => c.Properties)
+            .FirstOrDefault(prop => prop.Name.Equals("CodeAnalysisRules", StringComparison.OrdinalIgnoreCase))?.Value;
+
+        var sqlVersion = projects.SelectMany(p => p.ActiveConfigurations)
+            .SelectMany(c => c.Properties)
+            .FirstOrDefault(prop => prop.Name.Equals("SqlServerVersion", StringComparison.OrdinalIgnoreCase))?.Value;
+
+#pragma warning restore VSEXTPREVIEW_PROJECTQUERY_PROPERTIES_BUILDPROPERTIES // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+        runAnalyzer = runAnalyzer && projects.Count() == 1;
+
+        return (runAnalyzer, rules, sqlVersion);
     }
 }
 #pragma warning restore VSEXTPREVIEW_OUTPUTWINDOW // Type is for evaluation purposes only and is subject to change or removal in future updates.
---
+++ tools/SqlAnalyzerVsix/readme.md
@@ -0,0 +1,45 @@
+[marketplace]: https://marketplace.visualstudio.com/items?itemName=ErikEJ.TSqlAnalyzer
+[vsixgallery]: http://www.vsixgallery.com/extension/SqlAnalyzer.abc6ba2-edd5-4419-8646-a55d0a83f7ff/
+[repo]:https://github.com/ErikEJ/SqlServer.Rules
+
+# T-SQL Analyzer - Live Code Analysis for MSBuild.Sdk.Sqlproj SQL Projects
+
+[![Build](https://github.com/ErikEJ/SqlServer.Rules/actions/workflows/vsix.yml/badge.svg)](https://github.com/ErikEJ/SqlServer.Rules/actions/workflows/visx.yml)
+![GitHub Sponsors](https://img.shields.io/github/sponsors/ErikEJ)
+
+Download this extension from the [Visual Studio Marketplace][marketplace]
+or get the [CI build][vsixgallery]
+
+----------------------------------------
+
+Analyze your SQL code as you type, and get suggestions for improvements based on best practices. The analyzer has over 140 rules covering performance, security, maintainability, and more.
+
+![editor](art/editor.png)
+
+The extension will respect any rule configuration you have in your SQL project, including whether analysis is enabled, SQL version and rule suppression.
+
+```xml
+<Project Sdk="MSBuild.Sdk.SqlProj/3.2.0">
+  <PropertyGroup>
+    <TargetFramework>net8.0</TargetFramework>
+    <SqlServerVersion>Sql170</SqlServerVersion>
+    <RunSqlCodeAnalysis>True</RunSqlCodeAnalysis>
+    <CodeAnalysisRules>-SqlServer.Rules.SRD0006;-Smells.*</CodeAnalysisRules>
+  </PropertyGroup>
+</Project>
+```
+
+The extension also adds a menu item under `Tools` to run the T-SQL Analyzer tool against the currently open SQL script in the editor.
+
+![toolsmenu](art/toolsmenu.png)
+
+The extension works with both SQL Server and Azure SQL Database projects based on [MSBuild.Sdk.SqlProj](https://github.com/rr-wfm/MSBuild.Sdk.SqlProj).
+
+## How can I help?
+If you enjoy using the extension, please give it a ★★★★★ rating on the [Visual Studio Marketplace][marketplace].
+
+Should you encounter bugs or have feature requests, head over to the [GitHub repo][repo] to open an issue if one doesn't already exist.
+
+Another way to help out is to [sponsor me on GitHub](https://github.com/sponsors/ErikEJ).
+
+If you would like to contribute code, please fork the [GitHub repo][repo] and submit a pull request.
