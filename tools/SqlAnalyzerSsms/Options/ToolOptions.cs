18ec9ed5a6dd7e194a1ae6112484811bb3dcef41
Erik Ejlskov Jensen <eej@delegate.dk>
2026-06-13 14:14:07 +0200

    Make SSMS Vsix better

--- tools/SqlAnalyzerSsms/Options/ToolOptions.cs
+++ tools/SqlAnalyzerSsms/Options/ToolOptions.cs
@@ -6,15 +6,21 @@ namespace SqlAnalyzerSsms.Options
     public class ToolOptions : BaseOptionModel<ToolOptions>
     {
         [Category("General")]
+        [DisplayName(@"Check for updates")]
+        [Description("Check for updates to the extension on startup")]
+        [DefaultValue(true)]
+        public bool CheckForUpdates { get; set; } = true;
+
+        [Category("Code Analysis")]
         [DisplayName(@"Disable live code analysis")]
         [Description("Disable live static SQL code analysis")]
         [DefaultValue(false)]
         public bool DisableCodeAnalysis { get; set; }
 
-        [Category("General")]
-        [DisplayName(@"Check for updates")]
-        [Description("Check for updates to the extension on startup")]
-        [DefaultValue(true)]
-        public bool CheckForUpdates { get; set; } = true;
+        [Category("Code Analysis")]
+        [DisplayName(@"Code Analysis rule settings")]
+        [Description("Set the rules expression for live static SQL code analysis, for example: '-SqlServer.Rules.SRD0006;-SqlServer.Rules.SRN*'")]
+        [DefaultValue(null)]
+        public string? CodeAnalysisRuleSettings { get; set; }
     }
 }
