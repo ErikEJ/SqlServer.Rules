a74e8b756386d1da44100ad9dcdc7b801db2086d
Erik Ejlskov Jensen <eej@delegate.dk>
2026-09-01 07:51:16 +0200

    fix syntax

--- tools/SqlAnalyzerVsix/SqlAnalyzerExtension.cs
+++ tools/SqlAnalyzerVsix/SqlAnalyzerExtension.cs
@@ -18,7 +18,7 @@ public class SqlAnalyzerExtension : Extension
                 version: this.ExtensionAssemblyVersion,
                 publisherName: "ErikEJ",
                 displayName: "T-SQL Analyzer",
-                description: "T-SQL Analyzer, that analyzes your T-SQL scripts for design, naming and performance issues using more than 140 rules.")
+                description: "T-SQL Analyzer that analyzes your T-SQL scripts for design, naming and performance issues using more than 140 rules.")
         {
             MoreInfo = "https://github.com/ErikEJ/SqlServer.Rules",
             Tags = ["SQL", "T-SQL", "Analyzer", "SQL Server"],
